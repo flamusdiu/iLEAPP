150dc51ce62b421292ca0fb78f6025b46d297c61
Jesse Spangenberger <azulephoenix@gmail.com>
2021-01-19 15:36:59 -0500

    Updated Sphinx config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,17 +5,17 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Path setup --------------------------------------------------------------
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
 
 # Default Read The Docs themes
-import sphinx_rtd_theme
+import sphinx_rtd_theme  # noqa
+
+sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
 
 # -- Project information -----------------------------------------------------
 
